8c1526ce14fa8ef2e975b1d1225ba424d016cd27
migo <migo.vandingo@gmail.com>
2023-07-01 22:11:43 -0600

    delete

--- rtc3/src/index.js
+++ rtc3/src/index.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
+
 (window).global = window;
 (window).process = process;
 (window).Buffer = [];
